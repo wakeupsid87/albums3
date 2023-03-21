6997317c30945af5692d4eda6292d84c7d25c04e
Sidharth sharma <48231570+wakeupsid87@users.noreply.github.com>
2023-03-21 18:40:16 -0400

    Add files via upload

---
+++ albumsApi.js
@@ -0,0 +1,64 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.id }];
+        },
+        query: (album) => {
+          return {
+            method: "DELETE",
+            url: `/albums/${album.id}`,
+          };
+        },
+      }),
+      addAlbum: builder.mutation({
+        invalidatesTags: (results, error, user) => {
+          return [{ type: "UsersAlbums", id: user.id }];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
+      fetchAlbums: builder.query({
+        providesTags: (results, error, user) => {
+          const tags = results.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          return tags;
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
+export { albumsApi };
